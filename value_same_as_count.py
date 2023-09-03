fc1f4349893e85903b35d25bce5007b56082d28d
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-09-03 17:54:05 +0530

    n = int(input()) l = list(map(int,input().split()))
    res = []
    for i in l:
        if l.count(i) == i:
            if i not in res:
                res.append(i)
    print(len(res))

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -3,7 +3,7 @@ l = list(map(int,input().split()))
 
 res = []
 for i in l:
-    if i == l.count(i):
+    if l.count(i) == i:
         if i not in res:
             res.append(i)
-print(len(res))a
+print(len(res))

f8da57e1fd1bcbae61fd0e6e3a0bde0bfff41778
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-09-03 11:06:17 +0530

    n = int(input()) l = list(map(int,input().split()))
    res = []
    for i in l:
        if i == l.count(i):
            if i not in res:
                res.append(i)
    if res:
        print(* res)
    else:
        print(-1)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,11 +1,12 @@
 n = int(input())
 l = list(map(int,input().split()))
+
 res = []
 for i in l:
     if i == l.count(i):
         if i not in res:
             res.append(i)
 if res:
-    print(*res)
+    print(* res)
 else:
     print(-1)
